c18078241c3a3536774b26b2788ec76aa230a52e
fikri Riza Esfandiary <fikri.riza@gmail.com>
2023-06-29 23:16:34 +0800

    data produk harus autorization

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function __construct() {
-        $this->middleware('auth:api',['except' => ['index','show']]);
+        $this->middleware('auth:api',['except' => ['show']]);
     }
     public function index()
     {
